8c6ea7a2364b8767c99392857532e52ee2b0c60a
Vincenzo <v.nunziata16@studenti.unisa.it>
2017-01-17 12:45:11 +0100

    Query 3 ok

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -19,12 +19,26 @@ public class MainClass {
 			System.out.println(p.toString());
 		}
 		
+		System.out.println("");
 		ArrayList<Offerta> offerte = dh.getOffertePerPeriodo(Date.valueOf("2017-01-01"), Date.valueOf("2017-02-28"));
 		
 		for(Offerta o : offerte) {
 			System.out.println(o.toString());
 		}
 		
+		System.out.println("");
+		ArrayList<Offerta> offerte2 = dh.getOffertePerAttivita("L'enigma di Joker", true);
+		
+		for(Offerta o : offerte2) {
+			System.out.println(o.toString());
+		}
+		
+		System.out.println("");
+		ArrayList<Offerta> offerte3 = dh.getOffertePerAttivita("L'enigma di Joker", false);
+		
+		for(Offerta o : offerte3) {
+			System.out.println(o.toString());
+		}
 	}
 
 }
