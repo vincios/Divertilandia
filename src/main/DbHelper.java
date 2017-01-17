464e83f8739c9bb177163a59b113f54ef17fc938
TonyDauria94 <Antoniodauriadev@gmail.com>
2017-01-17 11:06:41 +0100

    Creata la Classe DbHelper

---
+++ src/main/DbHelper.java
@@ -0,0 +1,76 @@
+package main;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+import entita.ParcoDivertimenti;
+
+public class DbHelper {
+
+	private String url;
+	private String user;
+	private String password;
+	private Logger l;
+
+	public DbHelper() {
+		super();
+		
+		url = "jdbc:mysql://localhost:3306/divertilandia";
+		user = "root";
+		password = "roronoa";
+		l = Logger.getGlobal();
+	}
+	
+	public Connection connect() throws SQLException {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch(ClassNotFoundException e) {
+			l.log(Level.SEVERE, "Classe Mysql non trovata", e);
+		}
+	
+		return DriverManager.getConnection(url, user, password);
+	}
+	
+	public ArrayList<ParcoDivertimenti> getInfoParchiDivertimento() {
+		ArrayList<ParcoDivertimenti> parchi = new ArrayList<>();
+		try {
+			Connection connection = connect();
+			String query = "select * from parcodivertimenti p, contattotelefonico c where p.Nome = c.NomeParco order by p.Nome";
+			
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(query);
+			
+			String nome;
+			while (result.next()) {
+				nome = result.getString("Nome");
+				
+				if(!parchi.isEmpty() & nome.equals(parchi.get(parchi.size()-1))) {
+					String telefono = result.getString("Telefono");
+					parchi.get(parchi.size()-1).addNumeroTelefonico(telefono);
+				} else {
+				
+				String sede = result.getString("Sede");
+				String tipo = result.getString("Tipo");
+				int nBiglietti = result.getInt("NBiglietti");
+				String percorso = result.getString("Percorso");
+				String tema = result.getString("Tema");
+				int nPiscine = result.getInt("NPiscine");
+				String telefono = result.getString("Telefono");
+				
+				ParcoDivertimenti parco = new ParcoDivertimenti (nome, sede, tipo, nBiglietti, percorso, tema, nPiscine);
+				parco.addNumeroTelefonico(telefono);
+				
+				parchi.add(parco); 
+				}
+			}
+		} catch (SQLException e) {
+			l.log(Level.SEVERE, "Errore di connessione al DataBase\n" + e.getMessage(), e);
+		}
+		
+		return parchi;
+	}
+	
+}
